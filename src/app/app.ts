02e5f5719e77c188deefc560fba511c1ad0ea114
guifroes1984 <guilhermefroes26@gmail.com>
2026-04-23 16:42:21 -0300

    feat: adiciona template com async pipe e estados de loading e erro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserList } from './users/components/user-list/user-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [UserList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('attus-frontend-test');
 }
