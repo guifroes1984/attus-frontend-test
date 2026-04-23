893f30fbb5102684f3319ab097ad83622e621cc7
guifroes1984 <guilhermefroes26@gmail.com>
2026-04-23 16:18:41 -0300

    feat: adiciona filtro por nome no UsersService

--- src/app/users/services/users.ts
+++ src/app/users/services/users.ts
@@ -8,16 +8,20 @@ import { delay, Observable, of } from 'rxjs';
 export class Users {
   private users: User[] = [
     {
-      id: 1, 
-      nome: 'Davi Oliveira Froes', 
-      email: 'davi@email.com', 
-      cpf: '12345678900', 
+      id: 1,
+      nome: 'Davi Oliveira Froes',
+      email: 'davi@email.com',
+      cpf: '12345678900',
       telefone: '999999999',
       tipoTelefone: 'celular',
     },
   ];
 
-  getUsers(): Observable<User[]> {
-    return of(this.users).pipe(delay(500));
+  getUsers(term: string) {
+    const filtered = this.users.filter(user =>
+      user.nome.toLowerCase().includes(term.toLowerCase())
+    );
+
+    return of(filtered).pipe(delay(500));
   }
 }
